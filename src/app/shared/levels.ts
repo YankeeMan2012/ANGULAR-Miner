f88bd02ed8174d74bc91d7914277d7cf38ab5a69
Yankee <yankeeman2012@mail.ru>
2017-05-06 18:24:27 +0300

    [+] restructure

---
+++ src/app/shared/levels.ts
@@ -0,0 +1,34 @@
+export const levels = [
+    {
+        title: 'Новичок',
+        params: {
+            width: 9,
+            height: 9,
+            mines: 10
+        }
+    },
+    {
+        title: 'Любитель',
+        params: {
+            width: 16,
+            height: 16,
+            mines: 40
+        }
+    },
+    {
+        title: 'Профессионал',
+        params: {
+            width: 30,
+            height: 16,
+            mines: 99
+        }
+    },
+    {
+        title: 'Настроенный',
+        params: {
+            width: null,
+            height: null,
+            mines: null
+        }
+    },
+];
